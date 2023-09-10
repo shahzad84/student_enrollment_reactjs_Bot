f733a7554175b536a123cf3b77ce8f617c0b02f2
hilarious3 <shahzad20022002@gmail.com>
2023-09-10 12:49:40 +0530

    first bot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Chatbot from "react-simple-chatbot";
+import {Segment} from "semantic-ui-react"
 function App() {
+  const steps = [
+    {
+      id: "Greet",        
+      message: "Hello, Welcome to student info system!",        
+      trigger: "Done",        
+    },        
+    {       
+      id: "Done",        
+      message: "Please enter your name!",        
+      trigger: "waiting1",        
+    },      
+    {      
+      id: "waiting1",      
+      user: true,      
+      trigger: "Name",      
+    },      
+    {       
+      id: "Name",     
+      message: "Hi {previousValue}, Please select your issue",     
+      trigger: "issues",     
+    },      
+    {      
+      id: "issues",   
+      options: [   
+        {    
+          value: "React",   
+          label: "React",        
+          trigger: "React",       
+        },       
+        { value: "Angular", label: "Angular", trigger: "Angular" },       
+      ],       
+    },       
+    {       
+      id: "React",       
+      message:        
+        "Thanks for letting your React issue, Our team will resolve your issue ASAP",        
+      end: true,       
+    },       
+    {       
+      id: "Angular",       
+      message:       
+        "Thanks for letting your Angular issue, Our team will resolve your issue ASAP",       
+      end: true,       
+    },       
+  ]; 
+          
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Segment floated="right">
+        <Chatbot steps={steps} />
+      </Segment>
+    </>
   );
 }
 
